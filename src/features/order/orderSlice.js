faad874389ebbb70f49cc4bcc34a65ce548744aa
Mariyan Damyanov <60383290+marians1d@users.noreply.github.com>
2022-08-16 16:13:52 +0300

    Small update of names

--- src/features/order/orderSlice.js
+++ src/features/order/orderSlice.js
@@ -4,14 +4,17 @@ export const orderSlice = createSlice({
     name: 'order',
     initialState: [],
     reducers: {
-        addOrders: (state, action) => {
+        addAll: (state, action) => {
             state = action.payload.map(o => ({...o, comments: []}));
         },
-        addOrder: (state, action) => {
+        addOne: (state, action) => {
             state = [...state, action.payload ];
         },
-        addOrderDetails: (state, action) => {
+        addDetails: (state, action) => {
             state = state.map(x => x._id === action.orderId ? action.payload : x);
         },
+        remove: (state, action) => {
+            
+        }
     }
 });
