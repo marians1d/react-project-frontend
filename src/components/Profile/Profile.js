e53b7780d00d1c19bcf274077fd029123718559c
Mariyan Damyanov <marians1md@gmail.com>
2022-08-20 00:55:08 +0300

    Style profile page

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,15 +1,21 @@
 import { createRef } from 'react';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
+import { ClipLoader } from 'react-spinners';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 
 import styles from './Profile.module.css';
 
 import * as authService from '../../services/auth';
 import { updateUser } from '../../features/auth/authSlice';
+import { offLoading, onLoading } from '../../features/loading/loadingSlice';
+import classNames from 'classnames';
 
 export const Profile = () => {
     const dispatch = useDispatch();
     const user = useSelector(state => state.user);
+    const isLoading = useSelector(state => state.loading.user);
     const fileRef = createRef();
 
     const handleFile = (event) => {
@@ -18,24 +24,41 @@ export const Profile = () => {
 
 
     const handleUploadFile = (event) => {
+        dispatch(onLoading('user'));
+
         event.preventDefault();
         const data = new FormData();
         data.append('profileImage', event.target.files[0]);
         authService.setImage(data).then((response) => {
-            dispatch(updateUser({ profileImageUrl: response.profileImageUrl}));
+            dispatch(updateUser({ profileImageUrl: response.profileImageUrl }));
+            dispatch(offLoading('user'));
         });
     };
 
 
     return (
-        <div>
-            <div className={styles.image}>
-                <img src={user.profileImageUrl} alt="" />
-            </div>
+        <div className={styles.profile}>
+            <section className={styles['image-section']}>
+                <div className={styles.image}>
+                    <img src={user.profileImageUrl} alt={user.profileImageUrl} />
 
-            <input onChange={handleUploadFile} type={'file'} ref={fileRef} />
+                    <input className={styles.input} onChange={handleUploadFile} type={'file'} ref={fileRef} />
 
-            <button onClick={handleFile}>Постави</button>
+                    <button disabled={isLoading} className={classNames(styles.button, 'btn', 'btn-primary')} onClick={handleFile}><FontAwesomeIcon icon={faPenToSquare} /> Смени</button>
+
+                    {isLoading &&
+                        <div className={styles['spinner-background']}>
+                            <div className={styles.spinner} ><ClipLoader /></div>
+                        </div>
+                    }
+                </div>
+            </section>
+
+            <section className={styles.info}>
+                <p>Потребителско име: {user?.username}</p>
+                <p>Имейл: {user?.email}</p>
+            </section>
+
         </div>
     );
 };
