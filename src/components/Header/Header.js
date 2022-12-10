766c9bfaea79103e7460073664e7d2db78d94506
Mariyan Damyanov <60383290+marians1d@users.noreply.github.com>
2022-12-10 22:54:41 +0200

    Adding mobile header styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,23 @@
 import { NavLink, Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
 import styles from './Header.module.css';
 import { ProfileDropdown } from './ProfileDropdown/ProfileDropdown';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHamburger } from '@fortawesome/free-solid-svg-icons';
+import classNames from 'classnames';
 
 export const Header = () => {   
     const { isLogged } = useSelector(state => state.user);
 
+    const [activeNav, setActiveNav] = useState(false);
+
+    const toggleHandler = () => {
+        setActiveNav(oldState => !oldState);
+    };
+
     return (
-        <div className={styles['header-wrapper']}>
+        <div className={classNames(styles['header-wrapper'], { [styles.open]: activeNav })}>
             <header className={styles.header}>
                 <Link to="/" className={styles.logo}>
                     <h2>
@@ -15,7 +25,9 @@ export const Header = () => {
                     </h2>
                 </Link>
 
-                <ul className={styles.links}>
+                <FontAwesomeIcon onClick={toggleHandler} className={classNames(styles.hamburger)} icon={faHamburger} />
+
+                <ul className={classNames(styles.links)}>
                     <li>
                         <NavLink
                             className={({ isActive }) =>
@@ -63,9 +75,8 @@ export const Header = () => {
                             <Link className={'btn btn-primary'} to="/register">
                                 Регистрация
                             </Link>
-                        </>}
-
-
+                        </>
+                    }
                 </div>
             </header>
         </div>
